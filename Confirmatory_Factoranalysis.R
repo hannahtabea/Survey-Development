70dd3d8c74c5f6e56c51f59061fedf82ffb5cba9
Hannah Tabea Roos <hannahtabea.roos@gmail.com>
2021-11-30 20:54:46 +0100

    Model update
    Included reasonable models that could explain personality structures on big5 measures inspired by Chang, Connelly & Geeza (2012)

--- Confirmatory_Factoranalysis.R
+++ Confirmatory_Factoranalysis.R
@@ -10,8 +10,8 @@
 #--------------------------------------------------------------------------
 
 library(data.table)
-big5_huge <- fread('IPIP-FFM-data-8Nov2018.csv', select = seq(1:50))
-
+big5_huge <- read_delim("IPIP-FFM-data-8Nov2018.csv", 
+                                     "\t", escape_double = FALSE, trim_ws = TRUE) 
 recode_var <- function(x) (
   dplyr::recode(x,
          `1`= 5L,
@@ -21,15 +21,17 @@ recode_var <- function(x) (
          `5` = 1L)
 )
 
-library(hablar)
-big5 <- big5_huge %>% 
+set.seed(27)
+big5 <- big5_huge %>%
+        select(EXT1:OPN10) %>%
         sample_n(500) %>% 
-        retype() %>%
         mutate_at(c("EXT2","EXT4","EXT6","EXT8","EXT10",
-                    "EST2","EST4",
+                    "EST1","EST3","EST5","EST6","EST7","EST8","EST9","EST10",
                     "AGR1","AGR3","AGR5","AGR7",
                     "CSN2","CSN4","CSN6","CSN8",
-                    "OPN2","OPN4","OPN6"), recode_var)
+                    "OPN2","OPN4","OPN6"), 
+                  funs(recode(., `1`= 5L, `2`= 4L, `4`= 2L, `5`= 1L, .default = 3L)))
+str(big5)
 #---------------------------------------------------------------------------
 # prepare lavaan syntax
 #---------------------------------------------------------------------------
@@ -47,7 +49,6 @@ prep_formula <- function(abbrev) {
 }
 
 #save big5-specific item names
-set.seed(27)
 extra_names <- prep_formula(abbrev = "EXT")
 agree_names <- prep_formula(abbrev = "AGR")
 emo_names <- prep_formula(abbrev = "EST") 
@@ -96,7 +97,7 @@ big5_CFA <- cfa(model = big5_CFAmodel,
 # Fit the model to the data using robust standard errors
 #-----------------------------------------------------------------------------------
 summary(big5_CFA, fit.measures = TRUE,
-        standardized = TRUE)
+        standardized = TRUE, rsquare = TRUE)
 # interpretation for good model fit:
 # Chi-squared p-value <. 05 (depends on sample size)
 # Comparative Fit index (CFI) > .90
@@ -129,6 +130,26 @@ inspect(big5_CFA, "std")$lambda
 #fit statistics in standardized format
 standardizedsolution(big5_CFA)
 
+#------------------------------------------------------------------------------------
+# Adjust the model - COMMON METHOD VARIANCE
+#------------------------------------------------------------------------------------
+big5_CFAmodel_cmv <-'EXTRA =~ EXT1 + EXT2 + EXT3 + EXT4 + EXT5 + EXT7 + EXT8 + EXT9 + EXT10 
+                     AGREE =~ AGR1 + AGR2 + AGR4 + AGR5 + AGR6 + AGR7 + AGR8 + AGR9 + AGR10 
+                     EMO   =~ EST1 + EST2 + EST3 + EST5 + EST6 + EST7 + EST8 + EST9 + EST10 
+                     OPEN  =~ OPN1 + OPN2 + OPN3 + OPN5 + OPN6 + OPN7 + OPN8 + OPN9 + OPN10 
+                     CON   =~ CSN1 + CSN2 + CSN3 + CSN4 + CSN5 + CSN6 + CSN7 + CSN8 + CSN9  
+                     CMV   =~ EXT1 + EXT2 + EXT3 + EXT4 + EXT5 + EXT7 + EXT8 + EXT9 + EXT10 + AGR1 + AGR2 + AGR4 + AGR5 + AGR6 + AGR7 + AGR8 + AGR9 + AGR10 + CSN1 + CSN2 + CSN3 + CSN4 + CSN5 + CSN6 + CSN7 + CSN8 + CSN9 + EST1 + EST2 + EST3 + EST5 + EST6 + EST7 + EST8 + EST9 + EST10 + OPN1 + OPN2 + OPN3 + OPN5 + OPN6 + OPN7 + OPN8 + OPN9 + OPN10 '
+
+big5_CFA_cmv <- cfa(model = big5_CFAmodel_cmv,
+                        data = big5, estimator = "MLR")
+
+summary(big5_CFA_cmv, fit.measures = TRUE,
+        standardized = TRUE,  rsquare = TRUE)
+
+# get fit indices
+fitMeasures(big5_CFA_cmv,
+            fit.measures = c("cfi","tli", "rmsea"))
+
 
 #------------------------------------------------------------------------------------
 # Adjust the model - HIGHER ORDER MODEL
@@ -153,27 +174,51 @@ summary(big5_CFA_higher_order, fit.measures = TRUE,
 # get fit indices
 fitMeasures(big5_CFA_higher_order,
             fit.measures = c("cfi","tli", "rmsea"))
+#------------------------------------------------------------------------------------
+# Adjust the model - UNBALANCED HIERARCHY MODEL
+#------------------------------------------------------------------------------------
+
+# adjust model structure
+big5_CFAmodel_unbalanced<-'EXTRA =~ EXT1 + EXT2 + EXT3 + EXT4 + EXT5 + EXT7 + EXT8 + EXT9 + EXT10
+                             AGREE =~ AGR1 + AGR2 + AGR4 + AGR5 + AGR6 + AGR7 + AGR8 + AGR9 + AGR10
+                             EMO   =~ EST1 + EST2 + EST3 + EST5 + EST6 + EST7 + EST8 + EST9 + EST10
+                             OPEN  =~ OPN1 + OPN2 + OPN3 + OPN5 + OPN6 + OPN7 + OPN8 + OPN9 + OPN10
+                             CON   =~ CSN1 + CSN2 + CSN3 + CSN4 + CSN5 + CSN6 + CSN7 + CSN8 + CSN9
+                             EMO ~ AGREE + CON 
+                             EXTRA ~ OPEN'
+
+# # fit hopefully improved model
+big5_CFA_unbalanced <- cfa(model = big5_CFAmodel_unbalanced,
+                             data = big5, estimator = "MLR")
+
+summary(big5_CFA_unbalanced, fit.measures = TRUE,
+        standardized = TRUE, rsquare = TRUE)
+
+# get fit indices
+fitMeasures(big5_CFA_unbalanced,
+            fit.measures = c("cfi","tli", "rmsea"))
 
 #------------------------------------------------------------------------------------
 # Adjust the model - BLENDED MODEL
 #------------------------------------------------------------------------------------
 
-#EXTRA =~ AGR2 (I am interested in people.) AGR7 (I am not really interested in others.)
-#OPEN =~ CSN10 (I am exacting in my work.) EXT6 (I have little to say.)
-#AGREE =~ EST3 (I worry about things.) EXT3 (I feel comfortable around people.)
-#EMO =~ CSN4 (I make a mess of things.) AGR10 (I make people feel at ease.??) + AGR6 (I have a soft heart.)
-#CON =~ OPN7 (I am quick to understand things.) OPN9 (I spend time reflecting on things.)
+modi <- modificationIndices(big5_CFA, sort. = TRUE)
+#EXTRA =~  AGR7 (I am not really interested in others.) > AGR6(I have a soft heart.???) > EST10 (I often feel blue.???) > AGR4(I sympathize with others' feelings.???) > AGR2 (I am interested in people.)
+#OPEN =~ CSN10 (I am exacting in my work.) > EXT6 (I have little to say.)
+#AGREE =~ EXT8 (I don't like to draw attention to myself.) EXT5 (I worry about things.) EXT3 (I feel comfortable around people.)
+#EMO =~ CSN4 (I make a mess of things.) > AGR7 (I am not really interested in others.???) > AGR6 (I have a soft heart.) 
+#CON =~ AGR3(I insult people.) > EST3(I worry about things.???) OPN5(I have excellent ideas.) (I am quick to understand things.) OPN9 (I spend time reflecting on things.)
 big5_CFAmodel_blended <-'EXTRA =~ EXT1 + EXT2 + EXT3 + EXT4 + EXT5 + EXT7 + EXT8 + EXT9 + EXT10 + AGR2 + AGR7
-                         AGREE =~ AGR1 + AGR2 + AGR4 + AGR5 + AGR6 + AGR7 + AGR8 + AGR9 + AGR10 + EST3 + EXT3
-                         EMO   =~ EST1 + EST2 + EST3 + EST5 + EST6 + EST7 + EST8 + EST9 + EST10 + CSN4 + AGR6
+                         AGREE =~ AGR1 + AGR2 + AGR4 + AGR5 + AGR6 + AGR7 + AGR8 + AGR9 + AGR10 + EST8 
+                         EMO   =~ EST1 + EST2 + EST3 + EST5 + EST6 + EST7 + EST8 + EST9 + EST10 + AGR6
                          OPEN  =~ OPN1 + OPN2 + OPN3 + OPN5 + OPN6 + OPN7 + OPN8 + OPN9 + OPN10 + CSN10 + EXT6
-                         CON   =~ CSN1 + CSN2 + CSN3 + CSN4 + CSN5 + CSN6 + CSN7 + CSN8 + CSN9 + OPN7 + OPN9'
+                         CON   =~ CSN1 + CSN2 + CSN3 + CSN4 + CSN5 + CSN6 + CSN7 + CSN8 + CSN9 + EST3 '
 
 big5_CFA_blended <- cfa(model = big5_CFAmodel_blended,
                         data = big5, estimator = "MLR")
 
 summary(big5_CFA_blended, fit.measures = TRUE,
-        standardized = TRUE)
+        standardized = TRUE, rsquare = TRUE)
 
 # get fit indices
 fitMeasures(big5_CFA_blended,
@@ -190,6 +235,10 @@ fitMeasures(big5_CFA_blended,
 # fit index comparison - akaike information criterion = estimator of prediction error and thereby relative quality of statistical models for a given set of data.
 # ecvi = expected cross validation index = likelihood this model will replicate with the same sample size and population
 fitmeasures(big5_CFA_blended, c("aic","ecvi"))
+#fitmeasures(big5_CFA_higher_order, c("aic","ecvi"))
+fitmeasures(big5_CFA_unbalanced, c("aic","ecvi"))
+#fitmeasures(big5_CFA_cmv, c("aic","ecvi")) # winning model!
+
 # smaller than the original model?)
 fitmeasures(big5_CFA, c("aic","ecvi"))
 
@@ -201,34 +250,34 @@ fitmeasures(big5_CFA, c("aic","ecvi"))
 library(semPlot)
 # diagram model
 semPaths(big5_CFA, layout = "tree")
-semPaths(big5_CFA_higher, layout = "tree")
+semPaths(big5_CFA_blended, layout = "tree")
+semPaths(big5_CFA_cmv, layout = "tree")
 
 cormat_big5 <- cor(big5)
 library(corrplot)
 corrplot(cormat_big5, type = "upper", order = "hclust", 
-         tl.col = "black", tl.srt = 45, tl.cex = 0.5)
+         tl.col = "black", tl.srt = 45, tl.cex = 0.4)
 
 #------------------------------------------------------------------------------------
 # What if the model still did not converge? 
 #------------------------------------------------------------------------------------
 # get modification indices -> suggested changes on the model and what would happen to the estimates if it was added
 # change model one by one
-modificationindices(big5_CFA, sort = TRUE)
+#modificationindices(big5_CFA, sort = TRUE)
 
 # -> Heywood cases = correlationns between variables are out of bounds (sum up to > 1) or cfa includes negative variances
 # e.g. WARNING: covariance matrix of latent variables is not positive definite; use lavInspect(fit, "cov.lv") to investigate.
 #find the problem 
 # - are two of the latent variables so highly correlated they should be merged to a single one?
 # change formula
-summary(big5_CFA2, standardized = TRUE,
-        fit.measures = TRUE)
+
 
 # negative variances?
 # - are manifest variables non-normal/highly skewed?
 # - do the variables lie on a similar scale?
 
 # adjust for negative variances by specifying variance in the model manually
-as.data.frame(big5) %>% select(starts_with("AGR")) %>% var() %>% sum()
-as.data.frame(big5) %>% select(starts_with("EST")) %>% var() %>% sum()
-as.data.frame(big5) %>% select(starts_with("CSN")) %>% var() %>% sum()
+# as.data.frame(big5) %>% select(starts_with("AGR")) %>% var() %>% sum()
+# as.data.frame(big5) %>% select(starts_with("EST")) %>% var() %>% sum()
+# as.data.frame(big5) %>% select(starts_with("CSN")) %>% var() %>% sum()
 
